d87453cf4f1536e4d68d1156f578a64417ded77c
Sparsh Garg <sparshgarg15@gmail.com>
2023-01-30 15:54:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ try:
   else:
       #streamlit.write('The user entered ', fruit_choice)
       back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.text(fruityvice_normalized)
+      streamlit.text(back_from_function)
 
 except URLError as e:
     streamlit.error()
